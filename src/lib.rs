5fdbca79a49f9ccde26d16077c58fe65cb45cf5d
Angelo Carly <angelo.carly@protonmail.com>
2023-07-19 20:07:16 +0200

    Add vertex buffers

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,8 @@ struct State {
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 #[repr(C)]
@@ -43,9 +44,19 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] }, // Top
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] }, // Bottom Left
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] }, // Bottom Right
+    Vertex { position: [ 1.0, 0.0, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [ 0.5, 0.87, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [ -0.5, 0.87, 0.0], color: [0.5, 0.0, 1.0] },
+    Vertex { position: [ -1.0, 0.0, 0.0], color: [0.5, 1.0, 0.5] },
+    Vertex { position: [ -0.5, -0.87, 0.0], color: [1.0, 0.0, 0.5] },
+    Vertex { position: [ 0.5, -0.87, 0.0], color: [0.3, 0.0, 1.0] },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 2,
+    0, 2, 3,
+    0, 3, 4,
+    0, 4, 5,
 ];
 
 impl State {
@@ -75,7 +86,7 @@ impl State {
                     ..
                 },
                 ..
-            } => {}
+            } => {  }
             _ => {}
         }
 
@@ -114,7 +125,8 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer( 0, self.vertex_buffer.slice(..) );
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer( self.index_buffer.slice(..), wgpu::IndexFormat::Uint16 );
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
@@ -242,7 +254,15 @@ impl State {
             }
         );
 
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+        let num_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -253,7 +273,8 @@ impl State {
             size,
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 }
